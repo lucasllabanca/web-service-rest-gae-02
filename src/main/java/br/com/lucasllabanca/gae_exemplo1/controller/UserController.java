3e82f9ddfa80ea5cea344b1ddc095e2d038bd19c
lucasllabanca <lucasllabanca@hotmail.com>
2022-02-12 16:00:30 -0300

    Users controller finished with all methods

--- src/main/java/br/com/lucasllabanca/gae_exemplo1/controller/UserController.java
+++ src/main/java/br/com/lucasllabanca/gae_exemplo1/controller/UserController.java
@@ -1,16 +1,20 @@
 package br.com.lucasllabanca.gae_exemplo1.controller;
 
 import br.com.lucasllabanca.gae_exemplo1.exception.UserAlreadyExistsException;
+import br.com.lucasllabanca.gae_exemplo1.exception.UserNotFoundException;
 import br.com.lucasllabanca.gae_exemplo1.model.User;
 import br.com.lucasllabanca.gae_exemplo1.repository.UserRepository;
+import br.com.lucasllabanca.gae_exemplo1.util.CheckRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @RestController
@@ -21,6 +25,9 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    CheckRole checkRole;
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
@@ -41,10 +48,69 @@ public class UserController {
 
     @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     @PutMapping(path = "/byemail")
-    public ResponseEntity<User> updateUser(@RequestBody User user,
-                                           @RequestParam("email") String email,
-                                           Authentication authentication) {
+    public ResponseEntity<?> updateUser(@RequestBody User user,
+                                        @RequestParam("email") String email,
+                                        Authentication authentication) {
 
+        if (user.getId() != null && user.getId() != 0) {
+            boolean hasRoleAdmin = checkRole.hasRoleAdmin(authentication);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            if (hasRoleAdmin || userDetails.getUsername().equals(email)) {
+                if (!hasRoleAdmin) {
+                    user.setRole("ROLE_USER");
+                }
+
+                try {
+                    return new ResponseEntity<User>(userRepository.updateUser(user, email), HttpStatus.OK);
+                } catch (UserAlreadyExistsException e) {
+                    return new ResponseEntity<>(e.getMessage(), HttpStatus.PRECONDITION_FAILED);
+                } catch (UserNotFoundException e) {
+                    return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+                }
+            } else {
+                return new ResponseEntity<>("Usuário não autorizado", HttpStatus.FORBIDDEN);
+            }
+        }else {
+            return new ResponseEntity<>("Faltando parâmetro id", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/byemail")
+    public ResponseEntity<User> getUserByEmail(@RequestParam String email, Authentication authentication) {
+
+        boolean hasRoleAdmin = checkRole.hasRoleAdmin(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        if (hasRoleAdmin || userDetails.getUsername().equals(email)) {
+            Optional<User> optUser = userRepository.getByEmail(email);
+
+            if (optUser.isPresent()){
+                return new ResponseEntity<>(optUser.get(), HttpStatus.OK);
+            }else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @DeleteMapping(path = "/byemail")
+    public ResponseEntity<User> deleteUser(@RequestParam("email") String email, Authentication authentication) {
+        boolean hasRoleAdmin = checkRole.hasRoleAdmin(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        if (hasRoleAdmin || userDetails.getUsername().equals(email)) {
+
+            try {
+                return new ResponseEntity<>(userRepository.deleteUser(email), HttpStatus.OK);
+            } catch (UserNotFoundException e) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+        } else {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
     }
 
 }
